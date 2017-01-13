b2d700966c7fc6f758fd9e178e9bb56ba36c7afa
nissim <nissim.kurle@tekolutions.com>
2017-01-13 11:06:37 +0530

    to integrate google forms with android webview ,users will be filling up a google forms on an android tablet. End results will be stored in a google drive spreadsheet

--- app/src/main/java/com/prodyogic/researchstudy/MainActivity.java
+++ app/src/main/java/com/prodyogic/researchstudy/MainActivity.java
@@ -1,21 +1,8 @@
 package com.prodyogic.researchstudy;
 
-import android.app.Dialog;
-import android.app.ProgressDialog;
-import android.content.DialogInterface;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.view.Window;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
-import android.widget.Button;
-import android.widget.ProgressBar;
-import android.widget.Toast;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
