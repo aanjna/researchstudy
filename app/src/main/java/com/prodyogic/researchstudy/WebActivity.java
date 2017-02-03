48cd76cf3620aa240acd620c0f588ec9cbb98bf2
nissim <nissim.kurle@tekolutions.com>
2017-02-03 19:44:39 +0530

    Ini commit

--- app/src/main/java/com/prodyogic/researchstudy/WebActivity.java
+++ app/src/main/java/com/prodyogic/researchstudy/WebActivity.java
@@ -1,8 +1,12 @@
 package com.prodyogic.researchstudy;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class WebActivity extends AppCompatActivity {
 
@@ -16,6 +20,36 @@ public class WebActivity extends AppCompatActivity {
         ft.replace(R.id.your_placeholder, new FormFragment());
         ft.commit();
 
+    }
+    public void shareApp() {
+        try {
+            Intent i = new Intent(Intent.ACTION_SEND);
+            i.setType("text/plain");
+            i.putExtra(Intent.EXTRA_SUBJECT, "Research Survey");
+            String sAux = "\nLet me recommend you Install this cool application\n\n";
+            sAux = sAux + "https://play.google.com/store/apps/details \n\n";
+            i.putExtra(Intent.EXTRA_TEXT, sAux);
+            startActivity(Intent.createChooser(i, "choose one"));
+        } catch (Exception e) {
+            //e.toString();
+        }
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);//Menu Resource, Menu
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.share) {
+            Toast.makeText(getApplicationContext(), "share app", Toast.LENGTH_LONG).show();
+            shareApp();
+
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
